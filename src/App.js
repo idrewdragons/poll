79199b082dc13292fc1ff562a60a4b5668153082
Andrew Carey <andrewcarey1111@gmail.com>
2023-04-24 19:15:54 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./App.css";
+import Poll from "./components/Poll";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <Poll />
+      </header>
+    </div>
+  );
+}
+
+export default App;
